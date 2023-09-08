6cfda3e040537682006371acd3b0d0d9de862cc5
Angel Camargo <ACamargo@bancred.com.bo>
2023-09-08 14:52:05 -0400

    estructura basica de flask de la aplicacion

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("pirmer commit de prueba")
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return "corriendo el servidor base"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
